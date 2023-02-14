126d54db5a8a0ab9d6b0973598b00b0d9d42fcd4
pratyush-eightfold <pratyush.gupta@eightfold.ai>
2023-02-14 23:31:37 +0530

    feat: remove boilerplate and initiate project

---
+++ src/components/Heading.jsx
@@ -0,0 +1,12 @@
+import Logo from "../img/triangleLogo.png"
+import "../css/main.css"
+
+export default function Heading() {
+    return (
+    <>
+    <div className="wrapper">
+    <img src={Logo} width={21.65} height={18.75} />
+    </div>
+    </>
+    )
+    }
