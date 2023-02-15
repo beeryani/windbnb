3a86881111f86aef957b3801d2473853ea201185
pratyush-eightfold <pratyush.gupta@eightfold.ai>
2023-02-15 17:54:26 +0530

    commit: Add top dropdown box to main

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Heading from './components/Heading'
 import Searchbox from './components/Searchbox'
+import Topdrawer from './components/Topdrawer'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Heading />
     <Searchbox />
+    <Topdrawer />
   </React.StrictMode>,
 )
