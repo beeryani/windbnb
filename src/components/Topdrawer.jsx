61b2598b082864236da9f78026088ae190d3ab3c
pratyush-eightfold <pratyush.gupta@eightfold.ai>
2023-02-15 17:43:21 +0530

    feat: add dropdown drawer to search

---
+++ src/components/Topdrawer.jsx
@@ -0,0 +1,39 @@
+import { Button, Drawer, Radio, Space } from 'antd';
+import { useState } from 'react';
+import Searchbox from './Searchbox';
+
+const Topdrawer = () => {
+  const [open, setOpen] = useState(false);
+  const [placement, setPlacement] = useState('left');
+  const showDrawer = () => {
+    setOpen(true);
+  };
+  const onClose = () => {
+    setOpen(false);
+  };
+  const onChange = (e) => {
+    setPlacement(e.target.value);
+  };
+  return (
+    <>
+      <Space>
+        <Button type="primary" onClick={showDrawer}>
+          Open
+        </Button>
+      </Space>
+      <Drawer
+        title="Basic Drawer"
+        placement="top"
+        closable={false}
+        onClose={onClose}
+        open={open}
+        key="top"
+      >
+        <div className='dropdownsearchbox'>
+          <Searchbox />
+        </div>
+      </Drawer>
+    </>
+  );
+};
+export default Topdrawer;
